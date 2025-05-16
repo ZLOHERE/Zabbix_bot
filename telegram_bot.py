ee99334b75a145659e0703a7fdbed0a247283ea2
ZLO_HERE <sasha.tushentsov123@gmail.com>
2025-05-16 20:38:12 +0300

    message recieve

---
+++ telegram_bot.py
@@ -0,0 +1,26 @@
+from telegram import Bot
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+TOKEN = "7667391200:AAHu-N9aQv0OLFeTVxLrhZpwks4EXaku2XA"
+CHAT_ID = 1495147972
+bot = Bot(token=TOKEN)
+
+
+class ZabbixHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_lenght = int(self.headers['Content-Lenght'])
+        post_data = self.rfile.read(content_lenght)
+        try:
+            data = json.loads(post_data.decode())
+            message = f"Zabbix alert:\n{data.get('subject', '')}\n{data.get('message','')}"
+            bot.send_message(chat_id=CHAT_ID, text=message)
+            self.send_response(200)
+            self.end_headers()
+        except Exception as e:
+            self.send_response(500)
+            self.end_headers()
+
+if __name__ == "__main__":
+    print("Bot is running on port 8000....")
+    server = HTTPServer(('0.0.0.0', 8000), ZabbixHandler)
+    server.serve_forever()
